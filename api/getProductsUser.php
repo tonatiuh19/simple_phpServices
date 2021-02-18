5988249213178f4f45dda185fd8ab78e2be01bad
Gomez <tonatiuh.gomez@hp.com>
2021-02-17 20:44:23 -0600

    Update getProductsUser.php

--- api/getProductsUser.php
+++ api/getProductsUser.php
@@ -7,23 +7,27 @@ if($method == 'POST'){
 	$params= json_decode($requestBody);
 	$params = (array) $params;
 
-	if ($params['givenname'] && $params['date'] && $params['email']) {
-		$givenname = $params['givenname'];
+	if ($params['email']) {
 		$email = $params['email'];
 
-		$sql = "SELECT email FROM users WHERE email='".$email."'";
+		$sql = "SELECT d.id_products, e.price, d.name, d.description, d.id_country, i.country, y.quantity, y.email_user FROM products as d INNER JOIN countries as i on i.id_country=d.id_country INNER JOIN (SELECT a.id_prices, a.id_products, a.price FROM prices AS a WHERE date = ( SELECT MAX(date) FROM prices AS b WHERE a.id_products = b.id_products )) as e on d.id_products=e.id_products INNER JOIN (SELECT a.id_stock, a.id_products, a.quantity, a.date, a.email_user FROM stock AS a WHERE date = ( SELECT MAX(date) FROM stock AS b WHERE a.id_stock = b.id_stock ) and email_user='".$email."') as y on y.id_products=d.id_products WHERE d.active=1 and y.quantity>0 and d.id_country NOT IN ( SELECT id_country FROM countries WHERE id_country=10 ) ORDER BY y.quantity desc";
 		$result = $conn->query($sql);
 		if ($result->num_rows > 0) {
-
-		
+			//echo 'Hola';
+			//echo $result;
+			while($row = $result->fetch_assoc()) {
+				$array[] = array_map('utf8_encode', $row);
+			}
+			$res = json_encode($array, JSON_NUMERIC_CHECK);
+			header('Content-Type: application/json');
+			echo $res;
 		} else {
-
-
+			echo "No results";
 		}
 	}else{
 		echo "Not valid Body Data";
 	}
-	
+
 }else{
 	echo "Not valid Data";
 }
